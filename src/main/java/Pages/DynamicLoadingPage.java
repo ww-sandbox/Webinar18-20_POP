7757075e193640a2c0da964da04cf4fa7b95794c
Wojciech Wroblewski <wojciech.wroblewski@intive.com>
2021-02-28 23:10:35 +0100

    zmiany dla webinara

--- src/main/java/Pages/DynamicLoadingPage.java
+++ src/main/java/Pages/DynamicLoadingPage.java
@@ -3,8 +3,11 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DynamicLoadingPage extends BasePage {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DynamicLoadingPage.class);
     public DynamicLoadingPage(WebDriver driver){
         super(driver);
     }
@@ -19,24 +22,28 @@ public class DynamicLoadingPage extends BasePage {
     private WebElement finishTextHeader;
 
     public DynamicLoadingPage clickStartButton(){
+        LOGGER.debug("Uruchomienie ładowania");
         startButton.click();
 
         return this;
     }
 
     public DynamicLoadingPage checkLoadingStarted(){
+        LOGGER.debug("Pojawienie się loadera");
         waitForElementToBeVisible(loadingBar);
 
         return this;
     }
 
     public DynamicLoadingPage checkLoadingFinished(){
+        LOGGER.debug("Zniknięcie loadera");
         waitForElementToBeInvisible(loadingBar);
 
         return this;
     }
 
     public String finishLoadingConfirmation(){
+        LOGGER.debug("Pojawienie się tekstu końcowego");
         waitForElementToBeVisible(finishTextHeader);
 
         return finishTextHeader.getText();
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,24 +1,42 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import utils.DriverProvider;
+import utils.ScreenshotUtil;
 
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 
 public class BaseTest {
     WebDriver driver;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
 
     @BeforeClass
     public void setUp(){
-        System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
-        System.setProperty("webdriver.gecko.driver", "C:/webdrivers/geckodriver.exe");
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver = DriverProvider.getDriver();
     }
 
     @AfterClass
     public void tearDown(){
-        driver.quit();
+        DriverProvider.quitDriver();
+    }
+
+    @BeforeMethod
+    public void methodSetup(ITestResult result){
+        LOGGER.info("Rozpoczęcie testu " + result.getMethod().getMethodName());
+
+    }
+
+    @AfterMethod
+    public void methodTearDown(ITestResult result) throws IOException {
+        LOGGER.info("Zakończenie testu " + result.getMethod().getMethodName());
+        if(result.getStatus() != ITestResult.SUCCESS){
+            String screenshotName = "screenshots/" + result.getName() + result.getEndMillis() + ".png";
+            ScreenshotUtil.takeScreenshot(driver, screenshotName);
+        }
     }
 }
